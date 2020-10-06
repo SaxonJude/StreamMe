98acfb6906d47fec61302baebc0b44c231460392
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-10-06 22:51:09 +0100

    Create user.model.js

---
+++ backend/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    age: { type: Number, required: true },
+    location: { type: String }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
