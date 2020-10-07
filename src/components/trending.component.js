47871f6b38dbba0308b4185d6502700e20321a65
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-10-07 22:23:41 +0100

    Create trending.component.js

---
+++ src/components/trending.component.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavbarRed from './Navbar-red.component';
+
+class TrendingPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavbarRed />
+                <div className="profile">
+                    <div className="explore-wave-bot"></div>
+                    <div className="profile-container">
+                        <div className="sidebar">
+                        <div className="sidebar-content">
+                                <ul>
+                                    <Link to="/"><li><p>Home</p></li></Link>
+                                    <Link to="/explore"><li><p>Explore</p></li></Link>
+                                    <Link to="/trending"><li className="sidebar-active"><p>Trending<i class="fas fa-arrow-right"></i></p></li></Link>
+                                    <Link to="/series"><li><p>Series</p></li></Link>
+                                    <Link to="/films"><li><p>Films</p></li></Link>
+                                    <Link to="/latest"><li><p>Latest</p></li></Link>
+                                </ul>
+                            </div>
+                            <span className="white-bar"></span>
+                            <div className="sidebar-wave"></div>
+                        </div>
+                        <div className="explore-content">
+                            <h1>What's Trending</h1>
+                            <div className="newrelease-carousels">
+                                <section className="1"></section>
+                                <section className="2"></section>
+                            </div>
+                            <div className="page-buttons">
+                                <button><i class="fas fa-angle-left"></i></button>
+                                <button><i class="fas fa-angle-right"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TrendingPage;
