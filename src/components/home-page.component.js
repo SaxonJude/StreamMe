352a2167b22f6352f5d5e77bd822e0a9a638ed36
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-10-09 21:16:19 +0100

    Connected API to Home Page

--- src/components/home-page.component.js
+++ src/components/home-page.component.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import Navbar from './Navbar.component';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Navbar from './layout/Navbar.component';
+import { HomeMovie } from './movie-item.component';
 import wave from '../styles/images/wave.svg';
 import '../styles/style.css';
 
+import { APIKey } from '../APIKey';
+
+const UPCOMING_API = `https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKey}&language=en-US&page=1`;
+const POPULAR_API = `https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`;
+const NOWPLAYING_API = `https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKey}&language=en-US&page=1`;
+const TOPRATED_API = `https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKey}&language=en-US&page=1`;
+
 class HomePage extends React.Component {
+    state = { movies: [], upcoming: [], popular: [], nowplaying: [], toprated: [] };
+    
+    componentDidMount() {
+        const req1 = axios.get(UPCOMING_API);
+        const req2 = axios.get(POPULAR_API);
+        const req3 = axios.get(NOWPLAYING_API);
+        const req4 = axios.get(TOPRATED_API);
+
+        axios.all([req1, req2, req3, req4])
+        .then(axios.spread((...responses) => {
+            const upcoming = responses[0].data.results;
+            const popular = responses[1].data.results;
+            const nowplaying = responses[2].data.results;
+            const toprated = responses[3].data.results;
+            this.setState({ 
+                upcoming: upcoming.slice(0, 9),
+                popular: popular.slice(0, 9),
+                nowplaying: nowplaying.slice(0, 9),
+                toprated: toprated.slice(0, 9),
+             });
+        }));
+    }
+    
     render() {
+        const upcoming = this.state.upcoming;
+        const popular = this.state.popular;
+        const nowplaying = this.state.nowplaying;
+        const toprated = this.state.toprated;
         return (
             <div>
                 <Navbar />
@@ -17,6 +54,49 @@ class HomePage extends React.Component {
                     <div className="bottom_gradient"></div>
                 </div>
                 <div className="movie_list">
+                    <div className="movie-container">
+                        <div className="home-carousel">
+                            <h1><Link to="/upcoming">Upcoming</Link></h1>
+                            <div>
+                                {upcoming.length > 0 && upcoming.map(movie => {
+                                    return (<HomeMovie key={movie.id} {...movie}/>);
+                                })}
+                            </div>
+                        </div>
+                        <div className="divide-line">
+                            <span></span>
+                        </div>
+                        <div className="home-carousel">
+                            <h1><Link to="/popular">Popular</Link></h1>
+                            <div>
+                                {popular.length > 0 && popular.map(movie => {
+                                    return (<HomeMovie key={movie.id} {...movie}/>);
+                                })}
+                            </div>
+                        </div>
+                        <div className="divide-line">
+                            <span></span>
+                        </div>
+                        <div className="home-carousel">
+                            <h1><Link to="/now-playing">Now Playing</Link></h1>
+                            <div>
+                                {nowplaying.length > 0 && nowplaying.map(movie => {
+                                    return (<HomeMovie key={movie.id} {...movie}/>);
+                                })}
+                            </div>
+                        </div>
+                        <div className="divide-line">
+                            <span></span>
+                        </div>
+                        <div className="home-carousel">
+                            <h1><Link to="/top-rated">Top Rated</Link></h1>
+                            <div>
+                                {toprated.length > 0 && toprated.map(movie => {
+                                    return (<HomeMovie key={movie.id} {...movie}/>);
+                                })}
+                            </div>
+                        </div>
+                    </div>
                     <img src={wave} className="footer_wave" alt="footer wave" />
                 </div>
                 <footer>
@@ -41,10 +121,10 @@ class HomePage extends React.Component {
                         <div className="links">
                             <ul>
                                 <li>
-                                    <i class="fab fa-github-square"></i>
-                                    <i class="fab fa-linkedin"></i>
-                                    <i class="fab fa-codepen"></i>
-                                    <i class="fab fa-dribbble"></i>
+                                    <i className="fab fa-github-square"></i>
+                                    <i className="fab fa-linkedin"></i>
+                                    <i className="fab fa-codepen"></i>
+                                    <i className="fab fa-dribbble"></i>
                                 </li>
                                 <li>
                                     <p>Designed and Coded by Saxon Du Plooy.</p>
