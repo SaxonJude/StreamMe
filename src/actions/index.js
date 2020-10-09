8f8c789ac1bb52796dec41c64e5c43e42f94d4f4
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-10-09 21:12:12 +0100

    Add correct action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,33 @@
-export const increment = number => {
-    return {
-        type: 'INCREMENT',
-        payload: number
-    }
+import axios from 'axios';
+import { SEARCH_MOVIE, FETCH_MOVIES, FETCH_MOVIE } from './types';
+
+import { APIKey } from '../APIKey';
+
+export const searchMovie = text => dispatch => {
+    dispatch({
+        type: SEARCH_MOVIE,
+        payload: text // Search input value === action.payload
+    })
+};
+
+export const fetchMovies = text => dispatch => {
+    axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${APIKey}&query=${text}&page=1`)
+    .then(res => {
+        dispatch({
+            type: FETCH_MOVIES,
+            payload: res.data.results
+        })
+    })
+    .catch(err => console.log(err));
 }
 
-export const decrement = number => {
-    return {
-        type: 'DECREMENT',
-        payload: number
-    }
-}
+export const fetchMovie = id => dispatch => {
+    axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${APIKey}`)
+      .then(response =>
+        dispatch({
+          type: FETCH_MOVIE,
+          payload: response.data
+        })
+      )
+      .catch(err => console.log(err));
+  };
