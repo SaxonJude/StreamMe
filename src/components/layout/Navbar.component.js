7676346850410c8d92fe5a184b03def5ef039cd8
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-10-13 23:49:23 +0100

    Update Navbar.component.js

--- src/components/layout/Navbar.component.js
+++ src/components/layout/Navbar.component.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import SearchBar from './search-bar.component';
 
 class Navbar extends React.Component {
-    state = { navBackground: false };
+    state = { navBackground: false, toggle: false, isMobile: false };
 
     componentDidMount() {
         document.addEventListener("scroll", () => {
@@ -15,14 +15,26 @@ class Navbar extends React.Component {
             this.setState({ navBackground: false});
           }
         });
+
+        window.addEventListener('resize', () => {
+            this.setState({
+                isMobile: window.innerWidth <= 900
+            });
+        }, false);
+    }
+
+      onHamburgerClick = e => {
+          const toggle = !this.state.toggle;
+        this.setState({ toggle });
       }
 
     render() {
+        const isMobile = this.state.isMobile && this.state.toggle ? 'mobile' : '';
         return (
             <div>
                 <nav className={this.state.navBackground ? 'nav-1 active' : 'nav-1'}>
                     <div className="movie-list-1">
-                        <Link to="/" id="logo">Stream<span>Me</span></Link>
+                        <Link to="/" className="logo">Stream<span>Me</span></Link>
                         <ul>
                             <Link to="/">Home</Link>
                             <Link to="/upcoming">Upcoming</Link>
@@ -37,6 +49,12 @@ class Navbar extends React.Component {
                         </span>
                         <Link to="/explore"><i className="fas fa-compass"></i></Link>
                         <Link to="/account"><i className="fas fa-user"></i></Link>
+                    </div>
+                    <div className="hamburger" onClick={this.onHamburgerClick}><i class="fas fa-bars"></i></div>
+                    <div className={`footer_partial ${isMobile}`} >
+                        <Link to="/"><i class="fas fa-home"></i></Link>
+                        <Link to="/explore"><i className="fas fa-compass"></i></Link>
+                        <Link to="/user/account"><i className="fas fa-user"></i></Link>
                     </div>
                 </nav>
             </div>
