5abfe530a2c36450e2e4893806947630d29f7610
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-10-13 23:49:38 +0100

    Update Navbar-red.component.js

--- src/components/layout/Navbar-red.component.js
+++ src/components/layout/Navbar-red.component.js
@@ -2,14 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from './search-bar.component';
 
-
 class NavbarRed extends React.Component {
+    state = { toggle: false, isMobile: false };
+
+    componentDidMount() {
+        window.addEventListener('resize', () => {
+            this.setState({
+                isMobile: window.innerWidth <= 900
+            });
+        }, false);
+    }
+
+    onHamburgerClick = () => {
+        const toggle = !this.state.toggle;
+        this.setState({ toggle });
+    }
+
     render() {
+        const isMobile = this.state.toggle ? 'mobile' : '';
         return (
             <div>
                 <section className="nav-red">
                     <div className="movie-list">
-                        <div id="logo"><Link to="/">Stream<span>Me</span></Link></div>
+                        <Link className="logo" to="/">Stream<span>Me</span></Link>
                     </div>
                     <div className="user-list">
                         <span>
@@ -17,6 +32,12 @@ class NavbarRed extends React.Component {
                         </span>
                         <Link to="/explore"><i className="fas fa-compass"></i></Link>
                         <Link to="/account"><i className="fas fa-user"></i></Link>
+                    </div>
+                    <div className="hamburger" onClick={this.onHamburgerClick}><i class="fas fa-bars"></i></div>
+                    <div className={`footer_partial ${isMobile}`} >
+                        <Link to="/"><i class="fas fa-home"></i></Link>
+                        <Link to="/explore"><i className="fas fa-compass"></i></Link>
+                        <Link to="/user/account"><i className="fas fa-user"></i></Link>
                     </div>
                 </section>
             </div>
